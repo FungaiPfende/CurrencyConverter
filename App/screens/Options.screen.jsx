bf5f0a10235601a281470b5a7d21545100da51c5
kxdAmmo <fungaip163@outlook.com>
2022-01-10 16:52:18 +0200

    Set up Home and Options Screens Created RowItem & RowSeparator components Added Images

---
+++ App/screens/Options.screen.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { SafeAreaView, ScrollView, Linking, Alert } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+import colors from "../constants/colors";
+
+import { RowItem, RowSeparator } from "../components/RowItem.component";
+
+const openURL = (url) => {
+  return Linking.openURL(url).catch(() => {
+    Alert.alert("Sorry, something went wrong.", "Please try again later.");
+  });
+};
+
+export default () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <RowItem
+          text="Themes"
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+          onPress={() => alert("todo!")}
+        />
+
+        <RowSeparator />
+
+        <RowItem
+          text="React Native Basics"
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+          onPress={() =>
+            openURL(
+              "https://learn.reactnativeschool.com/p/react-native-basics-build-a-currency-converter"
+            )
+          }
+        />
+
+        <RowSeparator />
+
+        <RowItem
+          text="React Native by Example"
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+          onPress={() => openURL("https://reactnativebyexample.com")}
+        />
+
+        <RowSeparator />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
